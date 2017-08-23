bea7e9ab6d34ce3de1e7007dac488c717d34e43c
meta42 <648398613@qq.com>
2017-08-23 17:31:33 +0800

    hello

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -3,30 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using SimplexNoise;
 
+public enum BlockType
+{
+    None = 0,
+    Dirt = 1,
+    Grass = 3,
+    Gravel = 4,
+}
+
 [RequireComponent (typeof(MeshRenderer))]
 [RequireComponent (typeof(MeshCollider))]
 [RequireComponent (typeof(MeshFilter))]
 public class Chunk : MonoBehaviour {
 	
 	public static List<Chunk> chunks = new List<Chunk>();
-    public int width;
-    public int height;
-	public int[,,] map;
-    public int[,] heightMap;
-	public Mesh chunkMesh;
-	protected MeshRenderer meshRenderer;
-	protected MeshCollider meshCollider;
-	protected MeshFilter meshFilter;
+    public static int width = 30;
+    public static int height = 30;
+
+    public int seed;
+    public float baseHeight = 10;
+    public float frequency = 0.025f;
+    public float amplitude = 1;
+
+    BlockType[,,] map;
+    Mesh chunkMesh;
+	MeshRenderer meshRenderer;
+	MeshCollider meshCollider;
+	MeshFilter meshFilter;
 
     Vector3 offset0;
     Vector3 offset1;
     Vector3 offset2;
-    public int seed;
-    float baseHeight = 10;
-
-
-    public float frequency = 0.025f;
-    public float amplitude = 1;
 
     public static Chunk GetChunk(Vector3 wPos)
     {
@@ -51,29 +58,21 @@ public class Chunk : MonoBehaviour {
 		meshRenderer = GetComponent<MeshRenderer>();
 		meshCollider = GetComponent<MeshCollider>();
 		meshFilter = GetComponent<MeshFilter>();
-        Generate();
+        Init();
     }
 
-    void Update()
-    {
-        //Generate();
-    }
-
-
-    void Generate()
+    void Init()
     {
         Random.InitState(seed);
         offset0 = new Vector3(Random.value * 1000, Random.value * 1000, Random.value * 1000);
         offset1 = new Vector3(Random.value * 1000, Random.value * 1000, Random.value * 1000);
         offset2 = new Vector3(Random.value * 1000, Random.value * 1000, Random.value * 1000);
-        map = new int[width, height, width];
-        heightMap = new int[width, width];
-        GenerateHeightMap();
-        GenerateMap();
+        map = new BlockType[width, height, width];
+        InitMap();
         BuildChunk();
     }
 
-    void GenerateMap()
+    void InitMap()
     {
         for (int x = 0; x < width; x++)
         {
@@ -81,24 +80,13 @@ public class Chunk : MonoBehaviour {
             {
                 for (int z = 0; z < width; z++)
                 {
-                    map[x, y, z] = CalculateType(new Vector3(x, y, z) + transform.position);
+                    map[x, y, z] = GenerateBlockType(new Vector3(x, y, z) + transform.position);
                 }
             }
         }
     }
 
-    void GenerateHeightMap()
-    {
-        for(int x = 0; x < width; x++)
-        {
-            for(int z = 0; z < width; z++)
-            {
-                heightMap[x, z] = CalculateHeight(new Vector3(x, 0f, z) + transform.position);
-            }
-        }
-    }
-
-    int CalculateHeight(Vector3 wPos)
+    int GenerateHeight(Vector3 wPos)
     {
         float x0 = (wPos.x + offset0.x) * frequency;
         float y0 = (wPos.y + offset0.y) * frequency;
@@ -112,42 +100,36 @@ public class Chunk : MonoBehaviour {
         float y2 = (wPos.y + offset2.y) * frequency / 4;
         float z2 = (wPos.z + offset2.z) * frequency / 4;
 
-        float noise0 = Noise.Generate(x0, z0, y0) * amplitude * 6;
-        float noise1 = Noise.Generate(x1, z1, y1) * amplitude / 4;
-        float noise2 = Noise.Generate(x2, z2, y2) * amplitude * 8;
+        float noise0 = Noise.Generate(x0, z0, y0) * amplitude;
+        float noise1 = Noise.Generate(x1, z1, y1) * amplitude / 2;
+        float noise2 = Noise.Generate(x2, z2, y2) * amplitude / 4;
 
-        return Mathf.FloorToInt(noise0 + noise1 + noise2+ baseHeight);
+        return Mathf.FloorToInt(noise0 + noise1 + noise2 + baseHeight);
     }
 
-    int CalculateType(Vector3 wPos)
+    BlockType GenerateBlockType(Vector3 wPos)
     {
         //y坐标是否在Chunk内
-        if (wPos.y < 0 || wPos.y >= height)
+        if (wPos.y >= height)
         {
-            return 0;
+            return BlockType.None;
         }
 
-        float heightMapValue = CalculateHeight(wPos);
-
-        if (wPos.y < 3)
+        float genHeight = GenerateHeight(wPos);
+        if (wPos.y > genHeight)
         {
-            return 4;
+            return BlockType.None;
+        }
+        else if (wPos.y == genHeight)
+        {
+            return BlockType.Grass;
+        }
+        else if (wPos.y < genHeight && wPos.y > genHeight - 5)
+        {
+            return BlockType.Dirt;
         }
 
-        if (wPos.y == heightMapValue)
-        {
-            return 3;
-        }
-        else if (wPos.y < heightMapValue && wPos.y > heightMapValue - 8)
-        {
-            return 1;
-        }
-        else if (wPos.y > heightMapValue)
-        {
-            return 0;
-        }
-
-        return 3;
+        return BlockType.Gravel;
     }
 
 	public void BuildChunk()
@@ -182,33 +164,33 @@ public class Chunk : MonoBehaviour {
     {
         if (map[x, y, z] == 0) return;
 
-        int brick = map[x, y, z];
+        BlockType brick = map[x, y, z];
 
-        // Left
-        if (CheckBuildFace(x - 1, y, z))
+        //Left
+        if (CheckNeedBuildFace(x - 1, y, z))
             BuildFace(brick, new Vector3(x, y, z), Vector3.up, Vector3.forward, false, verts, uvs, tris);
-        // Right
-        if (CheckBuildFace(x + 1, y, z))
+        //Right
+        if (CheckNeedBuildFace(x + 1, y, z))
             BuildFace(brick, new Vector3(x + 1, y, z), Vector3.up, Vector3.forward, true, verts, uvs, tris);
 
-        // Bottom
-        if (CheckBuildFace(x, y - 1, z))
+        //Bottom
+        if (CheckNeedBuildFace(x, y - 1, z))
             BuildFace(brick, new Vector3(x, y, z), Vector3.forward, Vector3.right, false, verts, uvs, tris);
-        // Top
-        if (CheckBuildFace(x, y + 1, z))
+        //Top
+        if (CheckNeedBuildFace(x, y + 1, z))
             BuildFace(brick, new Vector3(x, y + 1, z), Vector3.forward, Vector3.right, true, verts, uvs, tris);
 
-        // Back
-        if (CheckBuildFace(x, y, z - 1))
+        //Back
+        if (CheckNeedBuildFace(x, y, z - 1))
             BuildFace(brick, new Vector3(x, y, z), Vector3.up, Vector3.right, true, verts, uvs, tris);
-        // Front
-        if (CheckBuildFace(x, y, z + 1))
+        //Front
+        if (CheckNeedBuildFace(x, y, z + 1))
             BuildFace(brick, new Vector3(x, y, z + 1), Vector3.up, Vector3.right, false, verts, uvs, tris);
 
 
     }
 
-    void BuildFace(int brick, Vector3 corner, Vector3 up, Vector3 right, bool reversed, List<Vector3> verts, List<Vector2> uvs, List<int> tris)
+    void BuildFace(BlockType brick, Vector3 corner, Vector3 up, Vector3 right, bool reversed, List<Vector3> verts, List<Vector2> uvs, List<int> tris)
 	{
         int index = verts.Count;
 		
@@ -247,63 +229,33 @@ public class Chunk : MonoBehaviour {
 		
 	}
 
-    bool CheckBuildFace (int x, int y, int z)
+    bool CheckNeedBuildFace (int x, int y, int z)
 	{
 		if ( y < 0) return false;
-		int type = GetLocalId(x,y,z);
+		var type = GetBlockType(x,y,z);
 		switch (type)
 		{
-		case 0: 
+		case BlockType.None: 
 			return true;
 		default:
 			return false;
 		}
 	}
 
-    public int GetLocalId(int x, int y, int z)
+    public BlockType GetBlockType(int x, int y, int z)
     {
-
         if (y < 0 || y > height - 1)
         {
             return 0;
         }
 
-        int id = 0;
         if ((x < 0) || (z < 0) || (x >= width) || (z >= width))
         {
-            id = GetWorldId(new Vector3(x, y, z) + transform.position);
+            var id = GenerateBlockType(new Vector3(x, y, z) + transform.position);
             return id;
         }
-
-        id = CalculateType(new Vector3(x, y, z) + transform.position);
-        return id;
+        return map[x,y,z];
     }
-
-    public int GetWorldId(Vector3 position)
-    {
-        if (position.y < 0f || position.y >= height)
-        {
-            return 0;
-        }
-        int id = 0;
-        Chunk chunk = GetChunk(position);
-        if (chunk != null)
-        {
-            Vector3 chunkPosition = chunk.transform.position;
-
-            int x = Mathf.FloorToInt(position.x - chunkPosition.x);
-            int y = Mathf.FloorToInt(position.y - chunkPosition.y);
-            int z = Mathf.FloorToInt(position.z - chunkPosition.z);
-
-            id = chunk.GetLocalId(x, y, z);
-        }
-        else
-        {
-            id = CalculateType(position);
-        }
-        return id;
-    }
-
 }
 
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,24 @@ public class Player : MonoBehaviour
 {
     CharacterController cc;
     public float speed = 20;
+    public float viewRange = 30;
+    public Chunk chunkPrefab;
 
     private void Start()
     {
         cc = GetComponent<CharacterController>();
     }
 
-    void Update () {
+    void Update ()
+    {
+        UpdateInput();
+        UpdateWorld();
+    }
+
+    void UpdateInput()
+    {
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
-
-       
 
         var x = Input.GetAxis("Mouse X");
         var y = Input.GetAxis("Mouse Y");
@@ -26,11 +33,29 @@ public class Player : MonoBehaviour
 
         if (Input.GetButton("Jump"))
         {
-            cc.Move((transform.right * h + transform.forward * v + transform.up )* speed * Time.deltaTime);
+            cc.Move((transform.right * h + transform.forward * v + transform.up) * speed * Time.deltaTime);
         }
         else
         {
             cc.SimpleMove(transform.right * h + transform.forward * v * speed);
+        }
+    }
+
+    void UpdateWorld()
+    {
+        for (float x = transform.position.x - viewRange; x < transform.position.x + viewRange; x += Chunk.width)
+        {
+            for (float z = transform.position.z - viewRange; z < transform.position.z + viewRange; z += Chunk.width)
+            {
+                Vector3 pos = new Vector3(x, 0, z);
+                pos.x = Mathf.Floor(pos.x / (float)Chunk.width) * Chunk.width;
+                pos.z = Mathf.Floor(pos.z / (float)Chunk.width) * Chunk.width;
+
+                Chunk chunk = Chunk.GetChunk(pos);
+                if (chunk != null) continue;
+
+                chunk = (Chunk)Instantiate(chunkPrefab, pos, Quaternion.identity);
+            }
         }
     }
 }
